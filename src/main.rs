23b9066e5d1b39a08c1a4a05117a157da13e9e64
Sune Andersson <nsn800@gmail.com>
2025-08-29 14:04:28 +0000

    avs 108

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,18 @@ fn next_language<'a>(languages: &'a [String], current: &str) -> &'a str {
     languages.last().unwrap()
 }
 
+fn last_language(languages: &[String]) -> &str {
+    languages.last().unwrap()
+}
+
+fn longest<'a, 'b>(one: &'a str, two: &'a str) -> &'a str {
+    if one.len() > two.len() {
+        one
+    } else {
+        two
+    }
+}
+
 fn main() {
     let languages = vec![
         String::from("rust"),
@@ -19,7 +31,12 @@ fn main() {
         String::from("typescript"),
     ];
 
-    let result = next_language(&languages, "go");
+    let result = next_language(&languages, "rust");
+    println!("{}", result);
 
+    let result = last_language(&languages);
+    println!("{}", result);
+
+    let result = longest("gokkk", "a");
     println!("{}", result);
 }
